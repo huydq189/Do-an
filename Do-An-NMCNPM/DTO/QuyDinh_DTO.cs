4cec345b74093742ee7d40457b8a1316cb38c97f
duc123 <vinhduc4498@gmail.com>
2018-06-27 20:23:25 +0700

    updateQuyDInh

---
+++ Do-An-NMCNPM/DTO/QuyDinh_DTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class QuyDinh_DTO
+    {
+        public int ID { get; set; }
+        public int TonToiThieu { get; set; }
+        public int SoLuongItNhat { get; set; }
+        public double NoToiDa { get; set; }
+        public int Ttts { get; set; }
+        public int QDThuTien = 1;
+    }
+}
